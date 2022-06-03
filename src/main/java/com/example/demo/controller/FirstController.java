bc8e3291f592393597f1613f2fc3780e60275e86
Matthew Kudelin <matthew@orca-ai.io>
2022-06-03 10:24:41 +0300

    init commit

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
 
+    //test
     @GetMapping(value = "/test")
     public String test(){
         return "walla!";
